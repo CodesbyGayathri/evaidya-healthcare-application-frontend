1ccde6e8dc4c024a4b51027e191ba4ce7813a915
CodesbyGayathri <codesbygj@gmail.com>
2024-10-23 20:24:58 -0500

    added header to all components

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  constructor(private fb: FormBuilder) {}
+  signupForm: FormGroup;
+  message: string = '';
+  ngOnInit(): void {
+    this.signupForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      gender: ['', Validators.required],
+      mobileNumber: ['', [Validators.required, Validators.pattern(/^\d+$/)]],
+      user: this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+      }),
+      address: this.fb.group({
+        aptNumber: ['', Validators.required],
+        street: ['', Validators.required],
+        city: ['', Validators.required],
+        state: ['', Validators.required],
+        zipCode: ['', [Validators.required, Validators.pattern(/^\d+$/)]],
+      })
+    });
+  }
+
+  onSubmit(): void {
+    
+  }
 
 }
